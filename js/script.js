b854875e733fbeb5be388822a91f282f1e9d7b1d
jin ah <dbwlsdkk9546@naver.com>
2023-08-11 11:37:51 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+(async () => {
+  const productImageElements = document.querySelectorAll("#productImage");
+  const productNameElements = document.querySelectorAll("#productName");
+  const productColorElements = document.querySelectorAll("#productColor");
+  const productPriceElements = document.querySelectorAll("#productPrice");
+
+  console.log(productColorElements);
+})();
